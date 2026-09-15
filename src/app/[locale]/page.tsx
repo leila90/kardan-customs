1017cd882cc67d357961f74e6beaaa61de682bd0
leila90 <yadegari.leila@gmail.com>
2026-09-15 17:06:44 +0330

    feat: add pre-action conversation to homepage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,19 +1,17 @@
-
 import ContactUs from "@/components/sections/contact/contact-us";
 import HeroFooter from "@/components/sections/footer/hero-footer";
-import Features from "@/components/sections/features/features";
+import PreActionConversation from "@/components/sections/pre-action-conversation/pre-action-conversation";
 import WhyUs from "@/components/sections/why-us/why-us";
-import KardanHero from "@/components/sections/kardan-hero/kardan-hero";
 import Hero from "@/components/sections/hero/hero";
 
 export default function Home() {
     return (
         <>
-            <Hero></Hero>
-            <HeroFooter/>
-            <Features/>
-            <WhyUs/>
-            <ContactUs/>
+            <Hero />
+            <HeroFooter />
+            <PreActionConversation />
+            <WhyUs />
+            <ContactUs />
         </>
     );
 }
